85f4d5964b2ebb9f15b6112fe0bdc8eb44fc6256
Raz Davidovich <razd@assembly.co.il>
2017-08-24 23:19:51 +0300

    updated the nuget package deployment

--- Assembly.Software.Utilities/Assembly.Software.Utilities/My Project/AssemblyInfo.vb
+++ Assembly.Software.Utilities/Assembly.Software.Utilities/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(True)> 
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("f0921acf-2666-4968-b544-42ba671ae094")> 
+<Assembly: Guid("f0921acf-2666-4968-b544-42ba671ae094")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.1.0")>
+<Assembly: AssemblyFileVersion("1.0.1.0")>
